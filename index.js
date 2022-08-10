1c577861778c7e4fe3efd3c5799de305d69b0789
toninofiamma <102326022+toninofiamma@users.noreply.github.com>
2022-08-10 15:26:44 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const camera = document.querySelector("#camera");
+const canvas = document.querySelector("canvas");
+const color = document.querySelector("#color");
+const ctx = canvas.getContext("2d");
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+            camera.srcObject = stream;
+        })
+        .catch(() => {
+            console.log("qualcosa è andato storto");
+        })
+}
+
+(function loop() {
+    requestAnimationFrame(loop)
+    setTimeout(() => {
+        ctx.drawImage(camera, 0, 0, canvas.width, canvas.height);
+        const rgb = ctx.getImageData(canvas.width / 2, canvas.height / 2, 1, 1).data;
+        console.log(rgb[0]);
+        color.style["background-color"] = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    }, 1000 / 60)
+})();
